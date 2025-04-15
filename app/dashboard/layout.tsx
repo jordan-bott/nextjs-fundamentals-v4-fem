c16a0f3911b630cd2517702aaec0df0066555690
jordan-bott <jordanbott.dev@gmail.com>
2025-04-15 13:11:04 -0700

    dashboard loading with loading file

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,9 +7,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
     <div className="min-h-screen">
       <Navigation />
       <main className="pl-16 md:pl-64 pt-0 min-h-screen">
-        <div className="max-w-6xl mx-auto p-4 md:p-8">
-          <Suspense fallback={<DashboardSkeleton />}>{children}</Suspense>
-        </div>
+        <div className="max-w-6xl mx-auto p-4 md:p-8">{children}</div>
       </main>
     </div>
   )
